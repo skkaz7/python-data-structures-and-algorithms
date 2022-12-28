6562bba02d7fdc0c590af298ba52f77b226fee95
skkaz7 <s.k.kazmierczak7@gmail.com>
2022-12-28 23:11:41 +0100

    optimized bubble sort

---
+++ optimized bubble sort.py
@@ -0,0 +1,20 @@
+def optimized_bubble_sort(num_list):
+    for i in range(len(num_list)):
+        swapped = False
+        for j in range(len(num_list) - i - 1):
+            if num_list[j] > num_list[j + 1]:
+                temp = num_list[j + 1]
+                num_list[j + 1] = num_list[j]
+                num_list[j] = temp
+
+                swapped = True
+
+        if not swapped:
+            break
+
+
+if __name__ == '__main__':
+    data = [-2, 45, 0, 11, -9]
+    optimized_bubble_sort(data)
+    print('Sorted Array in Ascending Order:')
+    print(data)
